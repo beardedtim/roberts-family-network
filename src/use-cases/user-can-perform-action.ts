f5ebb7980509593d249fe5311ef86c46cd62d350
Tim Roberts <timiroberts@gmail.com>
2022-03-15 18:44:45 -0400

    Allow Updating of Own Profile
    This updates our authorization check to take into account
    the UPDATE PROFILE command being sent.

--- src/use-cases/user-can-perform-action.ts
+++ src/use-cases/user-can-perform-action.ts
@@ -1,6 +1,6 @@
 import Log from '@app/monitoring/log'
 import viewRolesForUser from './view-roles-for-user'
-
+import findUserByAttribute from './find-user-by-attribute'
 const log = Log.child({
   'use-case': 'use-can-perform-action',
 })
@@ -16,9 +16,21 @@ const userCanPerformAction = async ({
 }) => {
   log.trace({ user, object, action }, 'Validating user')
   const roles = await viewRolesForUser(user)
+  const dbUser = await findUserByAttribute('id', user)
 
   if (roles.some(({ name }) => name === 'overlord' || name === 'admin')) {
     return true
+  }
+
+  // If this is an update action
+  if (action.indexOf('UPDATE') === 0) {
+    const [type, id] = object.split('::')
+
+    if (type === 'PROFILE') {
+      if (dbUser.id === id) {
+        return true
+      }
+    }
   }
 
   return false
